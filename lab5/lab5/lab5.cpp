2ccbac4de0e68a7907debe487f306faeb470be42
kirkin17 <kirkin17@users.noreply.github.com>
2019-12-25 01:39:22 +0300

    final

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -3,12 +3,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int A[3][2];
-	int B[2][3];
-	int C[3][3];
+	int A[3][2], B[2][3], C[3][3];
 
-	cout << "Введите элементы первой матрицы: ";
-	for (int i = 0; i < 3; i++) {
+	cout << "Введите первую матрицу: \n";
+	for (int i = 0; i < 3; i++) 
+	{
 		for (int j = 0; j < 2; j++)
 		{
 			cout << "A[" << i + 1 << "][" << j + 1 << "]= ";
@@ -16,7 +15,7 @@ int main()
 		}
 	}
 
-	cout << " Введите элементы второй матрицы: ";
+	cout << "Введите вторую матрицу: \n";
 	for (int i = 0; i < 2; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -26,20 +25,22 @@ int main()
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 3; i++) 
+	{
+		for (int j = 0; j < 3; j++) 
+		{
 			C[i][j] = 0;
-			for (int t = 0; t < 2; t++) {
+			for (int t = 0; t < 2; t++) 
+			{
 				C[i][j] += A[i][t] * B[t][j];
 			}
 		}
 	}
 
-	cout << "Матрица произведения";
+	cout << "Произведения: \n";
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 			cout << C[i][j] << " ";
 	}
-	return 0;
 }
